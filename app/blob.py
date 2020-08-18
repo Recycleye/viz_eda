b8828ccc42dbbcb685a844318266bfe9f806596a
Ricky Ma <mr.rickyma@gmail.com>
2020-08-18 16:34:27 -0700

    Setup for processing imgs from blob storage

--- app/blob.py
+++ app/blob.py
@@ -1,41 +1,68 @@
 import os
+from datetime import datetime, timedelta
+from pathlib import Path
 
-from azure.storage.blob import BlobServiceClient
+from azure.storage.blob import BlobServiceClient, ContainerSasPermissions, generate_container_sas
+from dotenv import load_dotenv
 from tqdm import tqdm
 
-# connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
-connect_str = (
-    "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;"
-    "AccountKey="
-    "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;"
-    "BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;"
-    "QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;"
-)
-# Create the BlobServiceClient object which will be used to get a container client
+# Load environment variables
+load_dotenv(dotenv_path=Path(".") / ".env")
+connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+account_name = os.getenv("AZURE_STORAGE_ACCOUNT_NAME")
+account_key = os.getenv("AZURE_STORAGE_ACCOUNT_ACCESS_KEY")
+# Create the BlobServiceClient object used to get a container client
 blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-container_name = "test"
+# Create the ContainerClient object used to get blob data
+container_name = "datastorage"
 container_client = blob_service_client.get_container_client(container_name)
 
 
-# def download_blobs(max_concurrency=1):
-# List the blobs in the container
-print("\nDownloading blobs...")
-blob_list = container_client.list_blobs()
-os.chdir("./blob_data")
-for blob in tqdm(blob_list):
-    blob_client = blob_service_client.get_blob_client(
-        container=container_name, blob=blob.name
+def get_datasets():
+    folders = container_client.walk_blobs(delimiter="/")
+    datasets = [f.name[:-1] for f in folders]
+    return datasets
+
+
+def get_blobs(dataset):
+    blob_list = container_client.list_blobs(name_starts_with=dataset)
+    return blob_list
+
+
+def get_blob_url(blob_name):
+    token = generate_container_sas(
+        account_name=account_name,
+        container_name=container_name,
+        blob_name=blob_name,
+        account_key=account_key,
+        permission=ContainerSasPermissions(read=True),
+        expiry=datetime.utcnow() + timedelta(hours=1),
     )
-    if "/" in "{}".format(blob.name):
-        head, tail = os.path.split("{}".format(blob.name))
-        download_file_path = os.path.join(os.getcwd(), head, tail)
-        download_file_folder = os.path.join(os.getcwd(), head)
-        if not os.path.isdir(download_file_folder):
-            print("Directory doesn't exist, creating it now")
-            print(download_file_folder)
-            os.makedirs(download_file_folder, exist_ok=True)
-    else:
-        download_file_path = blob.name
-    with open(download_file_path, "wb") as download_file:
-        file = blob_client.download_blob(max_concurrency=1)
-        download_file.write(file.readall())
+    url = (
+        f"https://{account_name}"
+        f".blob.core.windows.net/{container_name}/{blob_name}?{token}"
+    )
+    return url
+
+
+def download_blobs(blob_list):
+    # List the blobs in the container
+    print("\nDownloading blobs...")
+    os.chdir("./blob_data")
+    for blob in tqdm(blob_list):
+        blob_client = blob_service_client.get_blob_client(
+            container=container_name, blob=blob.name
+        )
+        if "/" in "{}".format(blob.name):
+            head, tail = os.path.split("{}".format(blob.name))
+            download_file_path = os.path.join(os.getcwd(), head, tail)
+            download_file_folder = os.path.join(os.getcwd(), head)
+            if not os.path.isdir(download_file_folder):
+                print("Directory doesn't exist, creating it now")
+                print(download_file_folder)
+                os.makedirs(download_file_folder, exist_ok=True)
+        else:
+            download_file_path = blob.name
+        with open(download_file_path, "wb") as download_file:
+            file = blob_client.download_blob(max_concurrency=1)
+            download_file.write(file.readall())
